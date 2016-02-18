0268f0145f578413ae9c6e7314e815c165503d2f
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-18 10:57:10 +0200

    submit button

--- app.php
+++ app.php
@@ -8,8 +8,8 @@
 
 <form method="get">
 	<label for="message">Message:* <label><br>
-	<input type="text" name=""message"><br>
-	<input type="submit" value"Save to DB">
+	<input type="text" name="message"><br>
+	<input type="submit" value="Save to DB">
 	
 
 <form>
