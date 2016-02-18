685185a84a26b9f996b4c05a40f1c548fbd162f6
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-18 10:53:14 +0200

    submit button

--- app.php
+++ app.php
@@ -1,1 +1,15 @@
-APP
+<?php
+
+
+
+?>
+
+<h2> First application </h2>
+
+<form method="get">
+	<label for="message">Message:* <label><br>
+	<input type="text" name=""message"><br>
+	<input type="submit" value"Save to DB">
+	
+
+<form>
