c77748ff45d013db179ad7024907989920057116
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-02-18 11:30:12 +0200

    message to who

--- app.php
+++ app.php
@@ -1,15 +1,24 @@
 <?php
 
-
+	//Getting the message from address
+	// if there is ?name=... then $_get["name"]
+	$my_message = $_GET["message"];
+	$who = $_GET["who"];
+	
+	echo "Message: ".$my_message." and is to ".$who;
 
 ?>
 
 <h2> First application </h2>
 
 <form method="get">
-	<label for="message">Message:* <label><br>
-	<input type="text" name="message"><br>
-	<input type="submit" value="Save to DB">
+	<label for="who">For who the message:* <label><br>
+	<input type="text" name="who"><br><br>
+
+	<label for="message">Type your Message here:* <label><br>
+	<input type="text" name="message"><br><br>
+	<input type="submit" value="Send"><br>
+	
 	
 
 <form>
